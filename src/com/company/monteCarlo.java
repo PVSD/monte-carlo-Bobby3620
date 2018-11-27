6b2b606512ce6a10158fa7dead2d9c6761c0c3fc
rd568 <bobbyday3620@gmail.com>
2018-11-27 11:22:09 -0500

    ay

---
+++ src/com/company/monteCarlo.java
@@ -0,0 +1,18 @@
+package com.company;
+import java.util.Random;
+
+/**
+ * Created by rd568 on 11/27/18.
+ */
+public class monteCarlo {
+    //State variables
+    public static double h, k, r;
+    private Random rndm = new Random();
+
+    public monteCarlo(double a, double b, double c){
+        h = a;
+        k = b;
+        r = c;
+    }
+
+}
