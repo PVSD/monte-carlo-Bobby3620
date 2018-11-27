dbe539ac94f52e50720a636bccd5b6ad064b4212
Bobby3620 <41356385+Bobby3620@users.noreply.github.com>
2018-11-27 18:36:32 -0500

    ay

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	    monteCarlo numbers = new monteCarlo();
+        monteCarlo numbers = new monteCarlo(10, 10, 5);
+        System.out.println(numbers.nextRainDrop_x());
+        System.out.println(numbers.nextRainDrop_y());
+        System.out.println(numbers.insideCircle());
     }
 }
--- src/com/company/monteCarlo.java
+++ src/com/company/monteCarlo.java
@@ -15,4 +15,25 @@ public class monteCarlo {
         r = c;
     }
 
+    public double nextRainDrop_x(){
+        int rand_x = rndm.nextInt((int)h) + 1;
+        return rand_x;
+    }
+
+    public double nextRainDrop_y(){
+        int rand_y = rndm.nextInt((int)k) + 1;
+        return rand_y;
+    }
+
+    public boolean insideCircle(){
+        double x = nextRainDrop_x();
+        double y = nextRainDrop_x();
+
+            if(x <= h && y <= k){
+                return true;
+            }else{
+                return false;
+            }
+    }
+
 }
